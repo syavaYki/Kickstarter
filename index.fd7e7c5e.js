abb22d83f0579db6185e99485b3b7f70a9f5030a
Seva Podolskiy <s_podolskiy@live.com>
2024-04-04 13:26:15 -0400

    make build

---
+++ index.fd7e7c5e.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{spaceBetween:30,slidesPerView:"1",effect:"slide",pagination:{el:".swiper-pagination",type:"fraction"},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
+//# sourceMappingURL=index.fd7e7c5e.js.map
